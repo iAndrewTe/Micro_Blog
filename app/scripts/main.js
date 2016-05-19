16770fe5edbd255a44c6a06cbfccecbfd77cb4bd
Aaron Te <aaronjte@gmail.com>
2016-05-19 01:19:31 -0400

    Working app, current problem: different post for each view

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,38 +19,39 @@ angular.module('PostApp', ['ngRoute', 'ngFileUpload'])
     .success(function(data) {
       $scope.posts = data;
     });
-  $scope.addPost= function () {
-    if ($scope.post.text != undefined && $scope.post.img != undefined) {
-      $scope.upload($scope.post.img, $scope.post.text)
-      $scope.post = {};
 
-      } else if ($scope.post.img == undefined) {
-        console.log('present!');
+  $scope.addPost= function (blog_id) {
+    $scope.post.blog_id = blog_id;
+      if ($scope.post.text != undefined && $scope.post.img != undefined) {
+        $scope.upload($scope.post.img, $scope.post.text, blog_id);
+
+      } else if ($scope.post.text != undefined) {
         Posts.create($scope.post)
           .success(function(data) {
-
           $scope.post = {};
           $scope.posts = data;
         });
     }
     };
 
-  $scope.upload = function (file, text) {
+  $scope.upload = function (file, text, blog_id) {
       Upload.upload({
-        url: '/api/blogposts',
-        data: {file: file, text: text}
-      }).then(function (resp) {
-        console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
-      }, function (resp) {
-        console.log('Error status: ' + resp.status);
+          url: '/api/blogposts',
+          data: {text: text, blog_id: blog_id},
+          file: file
+      }).then(function (res) {
+          $scope.post = {};
+          $scope.posts = res.data;
+      }, function (res) {
+          console.log('Error status: ' + res.status);
       }, function (evt) {
-        var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-        console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
-      });
+          var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+          console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+      })
     };
 
 
-  $scope.deletePost= function(id) {
+  $scope.deletePost = function(id) {
       Posts.delete(id)
           .success(function (data) {
               $scope.posts = data;
